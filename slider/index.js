e9ed170221d6b2c71c5a365992039b967c80531f
d <gitdavit@mail.ru>
2023-09-05 22:25:19 +0400

    slider

--- slider/index.js
+++ slider/index.js
@@ -11,9 +11,21 @@ let activeSlideIndex = 0;
 upBtn.addEventListener('click', () =>{
     changeSlide('up')
 });
+
 downBtn.addEventListener('click', () =>{
     changeSlide('down')
 });
+
+document.addEventListener('keydown', event =>{
+
+    if(event.key === 'ArrowUp'){
+        changeSlide('up');
+    }else if(event.key === 'ArrowDown'){
+        changeSlide('down');
+    }
+
+});
+
 
 function changeSlide(direction){
     if(direction === 'up'){
